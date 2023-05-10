8889313e386371ba951ec3df70551e7cafab0387
blazingh <hadibaal@DESKTOP-7001VT7>
2023-05-10 18:09:40 +0300

    reconfigured copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,8 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+  { import = "astrocommunity.pack.php" },
+  
 }
