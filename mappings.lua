23ec21176615ed58cb208c52b9fc4c422bffbe03
blazingh <hadibaalbaki2@gmail.com>
2023-05-15 09:19:12 +0300

    added system clipboard copy

--- mappings.lua
+++ mappings.lua
@@ -28,5 +28,6 @@ return {
   v = {
     ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
     ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
+    ["<leader>y"] = { ":w !xclip -selection clipboard<CR>", desc = "copy to system clipboard" },
   },
 }
